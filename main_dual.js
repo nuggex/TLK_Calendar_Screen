ba074d0e091101ee1b607ffe0b4af8985b74f6e1
nuggex <nuggex@gmail.com>
2020-10-07 11:51:26 +0300

    I have no idea what's going on

--- main_dual.js
+++ main_dual.js
@@ -234,12 +234,17 @@ function getTimePadding(inTime) {
 function getSauna() {
 
     console.log("asdasd");
-    Date.prototype.getWeek = function () {
-        var onejan = new Date(this.getFullYear(), 0, 1);
-        var today = new Date(this.getFullYear(), this.getMonth(), this.getDate());
-        var dayOfYear = ((today - onejan + 86400000) / 86400000);
-        return Math.ceil(dayOfYear / 7)
-    };
+    Date.prototype.getWeek = function() {
+        var date = new Date(this.getTime());
+        date.setHours(0, 0, 0, 0);
+        // Thursday in current week decides the year.
+        date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+        // January 4 is always in week 1.
+        var week1 = new Date(date.getFullYear(), 0, 4);
+        // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+        return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                              - 3 + (week1.getDay() + 6) % 7) / 7);
+      };
     let thisweek = new Date();
     let house = "This week House Sauna for Men & Roof Sauna for Women";
     let roof = "This week Roof Sauna for Men & House Sauna for Women";
